019f19dbb84ffcbb6cdb96afb31c67519d6db619
talhamuhammadashraf <talhamuhammadashraf@gmail.com>
2020-01-07 23:15:56 +0500

    about

--- src/app/about.js
+++ src/app/about.js
@@ -1,10 +1,11 @@
 import React ,{Component} from 'react'
 
-export default ()=>(
-    <div id="acontainer">
-    <div id='picContainer'>
-        <img src={require('../assets/favicon.jpeg')} id="profilepic"/>
-    </div>
-    <div id="aboutme"></div>
-    </div>
-)
+// export default ()=>(
+//     <div id="acontainer">
+//     <div id='picContainer'>
+//         <img src={require('../assets/favicon.jpeg')} id="profilepic"/>
+//     </div>
+//     <div id="aboutme"></div>
+//     </div>
+// )
+export default  ()=><div>Under Construction</div>
