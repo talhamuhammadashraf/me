85d191d2f48e07d09b32b76630478f9cb52ac33a
talhamuhammadashraf <talhamuhammadashraf@gmail.com>
2020-01-07 23:13:15 +0500

    Updates

---
+++ precache-manifest.ab2a4cc891bd459de48ef9587c8229b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bc9906c2fca8800dedac4dc8088671",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "0a43f26df183f0a02b57",
+    "url": "/me/static/css/main.3cdef895.chunk.css"
+  },
+  {
+    "revision": "2c088d8cf0b98fd60688",
+    "url": "/me/static/js/2.48dcb86d.chunk.js"
+  },
+  {
+    "revision": "0a43f26df183f0a02b57",
+    "url": "/me/static/js/main.9ea1224a.chunk.js"
+  },
+  {
+    "revision": "b8567914e1efdcbb5d33",
+    "url": "/me/static/js/runtime-main.310bbfa0.js"
+  },
+  {
+    "revision": "d4b21d437e0adf146531a1047eb0dd07",
+    "url": "/me/static/media/auovs.d4b21d43.png"
+  },
+  {
+    "revision": "c0704543ae87ba7dca63c6c2a4cb6592",
+    "url": "/me/static/media/deals.c0704543.png"
+  },
+  {
+    "revision": "bfe882343f6b2b32edd702d93cdf3b24",
+    "url": "/me/static/media/khaadi.bfe88234.png"
+  }
+]);
